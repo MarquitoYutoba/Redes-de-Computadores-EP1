c35fcdea02b78c18c9bbd9d669463669be692557
Denilson-sts <denilson.sts.j@gmail.com>
2025-11-02 23:29:04 -0300

    ajustes finais

--- hangman-client.py
+++ hangman-client.py
@@ -1,17 +1,15 @@
 import socket
 import sys
 
-
 # Função para enviar mensagem para o servidor
-def enviar_msg_server(socket_do_cliente, mensagem_base):
-    mensagem_completa = (mensagem_base + "\r\n").encode('ascii')
-    socket_do_cliente.sendall(mensagem_completa)
-
+def enviar_msg_server(socket_client, mensagem_base):
+    mensagem_completa = (mensagem_base + '\r\n').encode('ascii')
+    socket_client.sendall(mensagem_completa)
 
 # Função para receber mensagem do servidor
-def receber_msg_server(socket_do_cliente, buffer_do_cliente):
+def receber_msg_server(socket_client, buffer_do_cliente):
     while True:
-        dados = socket_do_cliente.recv(1024)
+        dados = socket_client.recv(1024)
 
         if not dados:
             return None, buffer_do_cliente
@@ -21,93 +19,120 @@ def receber_msg_server(socket_do_cliente, buffer_do_cliente):
         if b'\r\n' in buffer_do_cliente:
             indice_terminador = buffer_do_cliente.find(b'\r\n')
             msg_bytes = buffer_do_cliente[:indice_terminador]
-            msg_string = msg_bytes.decode('ascii',errors="replace")
+            msg_string = msg_bytes.decode('ascii',errors='replace')
             buffer_do_cliente = buffer_do_cliente[indice_terminador + 2:]
 
             return msg_string, buffer_do_cliente
 
-def Contem_invalido():
-    return
+def fechar_conexao(socket_client):
+    socket_client.close()
 
-#criar uma função para fechar a conexão
-def E_INVALID_FORMAT():
-    return
+def Contem_invalido(mensagem):
+    mensagem = mensagem.split()
+    for x in mensagem:
+        if not mensagem.isalnum():
+            return True
+        
+def Valida_espaco(mensagem):
+    palavras = mensagem.split()
+    if len(palavras) == 1:
+        return mensagem.count(' ') == 0
+    return len(palavras)-1 == mensagem.count(' ')
 
-def E_UNEXPECTED_MESSAGE(esperado, mensagem, servidor):
-    if mensagem != esperado  and mensagem.find("ERROR") > -1:
-        enviar_msg_server(servidor, "ERROR UNEXPECTED_MESSAGE")
-        return
+def E_INVALID_FORMAT(mensagem_original, socket_client):
+    if Contem_invalido(mensagem_original) or Valida_espaco(mensagem_original):
+        enviar_msg_server(socket_client, 'ERROR INVALID_FORMAT')
+        fechar_conexao(socket_client)
+        sys.exit('ERROR INVALID_FORMAT')
+
+def E_UNEXPECTED_MESSAGE(esperado, mensagem, socket_client):
+    if mensagem != esperado and mensagem.find('ERROR') == -1:
+        enviar_msg_server(socket_client, 'ERROR UNEXPECTED_MESSAGE')
+        fechar_conexao(socket_client)
+        sys.exit('ERROR UNEXPECTED_MESSAGE')
 
 nome = sys.argv[1]
-if len(sys.argv) > 2:
+if len(sys.argv) == 3:
     host_server = sys.argv[2].split(':')[0]
     porta_server = int(sys.argv[2].split(':')[1])
+elif len(sys.argv) > 3:
+    entrada = ''
+    entrada = entrada.join(sys.argv)
+    nome = entrada[entrada.find(' ')+1:entrada.rfind(' ')-1]
+    endereco = entrada[entrada.rfind(' '):]
+    host_server = endereco.split(':')[0]
+    porta_server = int(endereco.split(':')[1])
 else:
     host_server = '127.0.0.1'
     porta_server = 6891
 
-print("Conectando ao servidor...")
+print('Conectando ao servidor...')
 socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_client.connect((host_server, porta_server))
 buffer_recebimento = b''
 
-enviar_msg_server(socket_client, f"NEWPLAYER {nome}")
+enviar_msg_server(socket_client, f'NEWPLAYER {nome}\n')
 mensagem, buffer_recebimento = receber_msg_server(socket_client, buffer_recebimento)
-if mensagem != 'STANDBY':
-    sys.exit(0)
-print("Aguardando o jogo começar...")
+E_INVALID_FORMAT(mensagem, socket_client)
+E_UNEXPECTED_MESSAGE('STANDBY', mensagem, socket_client)
+print('Aguardando o jogo começar...')
 
 mensagem, buffer_recebimento = receber_msg_server(socket_client, buffer_recebimento)
+E_INVALID_FORMAT(mensagem, socket_client)
 partes = mensagem.split()
 if partes[0] == 'MASTER':
-    print("Você é o mestre do jogo!")
-    palavra = str(input("Digite a palavra: "))
-    enviar_msg_server(socket_client, f"WORD {palavra}")
+    print('Você é o mestre do jogo!')
+    palavra = str(input('Digite a palavra: '))
+    enviar_msg_server(socket_client, f'WORD {palavra}')
     mensagem, buffer_recebimento = receber_msg_server(socket_client, buffer_recebimento)
-    if mensagem != 'OK':
-        sys.exit(0)
+    E_INVALID_FORMAT(mensagem, socket_client)
+    E_UNEXPECTED_MESSAGE('OK', mensagem, socket_client)
     mensagem, buffer_recebimento = receber_msg_server(socket_client, buffer_recebimento)
     partes = mensagem.split()
 
-if partes[0] == 'NEWGAME':
-    print("Jogo iniciado!")
-    vidas = int(partes[1])
-    tamanho_palavra = int(partes[2])
-    print(f"Vidas para advinhar: {vidas}")
-    print(f"Tamanho da palavra: {tamanho_palavra}.")
-    print()
-else:
-    sys.exit(0)
+E_INVALID_FORMAT(mensagem, socket_client)
+E_UNEXPECTED_MESSAGE('NEWGAME', mensagem.split[0], socket_client)
+print('Jogo iniciado!')
+vidas = int(partes[1])
+tamanho_palavra = int(partes[2])
+print(f'Vidas para advinhar: {vidas}')
+print(f'Tamanho da palavra: {tamanho_palavra}.')
+print()
 
 letras_incorretas = []
 palavras_incorretas = []
 while True:
     mensagem, buffer_recebimento = receber_msg_server(socket_client, buffer_recebimento)
+    E_INVALID_FORMAT(mensagem, socket_client)
     partes = mensagem.split()
     if partes[0] == 'YOURTURN':
-        palpite = str(input(r"Digite sua jogada (letra ou palavra), ou \q para sair: "))
+        palpite = str(input(r'Digite sua jogada (letra ou palavra), ou \q para sair: '))
         if palpite == r'\q':
-            enviar_msg_server(socket_client, "QUIT")
+            enviar_msg_server(socket_client, 'QUIT')
             mensagem, buffer_recebimento = receber_msg_server(socket_client, buffer_recebimento)
-            if mensagem == 'OK':
-                sys.exit(0)
+            E_INVALID_FORMAT(mensagem, socket_client)
+            E_UNEXPECTED_MESSAGE('OK', mensagem, socket_client)
+            sys.exit(0)
         if len(palpite) == 1:
-            enviar_msg_server(socket_client, f"GUESS LETTER {palpite}")
+            enviar_msg_server(socket_client, f'GUESS LETTER {palpite}')
             mensagem, buffer_recebimento = receber_msg_server(socket_client, buffer_recebimento)
+            E_INVALID_FORMAT(mensagem, socket_client)
         else:
-            enviar_msg_server(socket_client, f"GUESS WORD {palpite}")
+            enviar_msg_server(socket_client, f'GUESS WORD {palpite}')
             mensagem, buffer_recebimento = receber_msg_server(socket_client, buffer_recebimento)
+            E_INVALID_FORMAT(mensagem, socket_client)
 
-        if mensagem == 'OK':
-            mensagem, buffer_recebimento = receber_msg_server(socket_client, buffer_recebimento)
-            partes = mensagem.split()
+        E_UNEXPECTED_MESSAGE('OK', mensagem, socket_client)
+        mensagem, buffer_recebimento = receber_msg_server(socket_client, buffer_recebimento)
+        E_INVALID_FORMAT(mensagem, socket_client)
+        partes = mensagem.split()
 
     if partes[0] == 'GAMEOVER':
-        print("O jogo terminou.")
+        print('O jogo terminou.')
         if partes[1] == 'WIN':
-            print(f"A palavra {partes[3]} foi adivinhada por {partes[2]}!")
+            print(f'A palavra {partes[3]} foi adivinhada por {partes[2]}!')
         elif partes[1] == 'LOSE':
-            print(f"A palavra {partes[3]} não foi adivinhada. Último palpite por: {partes[2]}")
+            print(f'A palavra {partes[3]} não foi adivinhada. Último palpite por: {partes[2]}')
         break
 
     elif partes[0] == 'STATUS':
@@ -120,6 +145,6 @@ while True:
                     letras_incorretas.append(partes[4])
                     letras_incorretas.sort()
             vidas = int(partes[1])
-        print(f"Letras erradas: {' '.join(letras_incorretas)}")
-        print(f"Palavras erradas: {' '.join(palavras_incorretas)}")
-        print(f"Jogador {partes[3]} fez uma jogada: {partes[4]}. Restam {partes[1]} vidas")
+        print(f'Letras erradas: {' '.join(letras_incorretas)}')
+        print(f'Palavras erradas: {' '.join(palavras_incorretas)}')
+        print(f'Jogador {partes[3]} fez uma jogada: {partes[4]}. Restam {partes[1]} vidas')
--- hangman-server.py
+++ hangman-server.py
@@ -16,7 +16,7 @@ def ler_msg_completo(socket_do_cliente, buffer_do_cliente):
         if b'\r\n' in buffer_do_cliente:
             indice_terminador = buffer_do_cliente.find(b'\r\n')
             msg_bytes = buffer_do_cliente[:indice_terminador]
-            msg_string = msg_bytes.decode('ascii',errors="replace")
+            msg_string = msg_bytes.decode('ascii',errors='replace')
             buffer_do_cliente = buffer_do_cliente[indice_terminador + 2:]
 
             return msg_string, buffer_do_cliente
@@ -28,61 +28,76 @@ def fechar_conexao(clientes_conectados):
         client['socket'].close()
 
 # Função para mandar uma certa mensagem para todos os clientes
-#Incluir o "\r\n" na função e enviar apenas as strings simplificadas no cast
 def mensagem_todos_clientes(clientes_conectados, message):
     for client in clientes_conectados:
         client['socket'].send(message.encode('ascii'))
 
 def Contem_invalido(mensagem):
-    return
+    mensagem = mensagem.split()
+    for x in mensagem:
+        if not mensagem.isalnum():
+            return True
+        
+def Valida_espaco(mensagem):
+    palavras = mensagem.split()
+    if len(palavras) == 1:
+        return mensagem.count(' ') == 0
+    return len(palavras)-1 == mensagem.count(' ')
 
-#avaliar como o player vai ser usado, para fechar a conexao precisa da lista de jogadores mas eu também preciso do jogador atual.
-def E_INVALID_FORMAT(player, mensagem):
-    if mensagem.find("?") > -1 or len(mensagem.split(" ")/mensagem.count) != 1:
-        fechar_conexao(player)
-        sys.exit("ERROR INVALID_FORMAT")
+def E_INVALID_FORMAT(mensagem_original, player_atual):
+    if Contem_invalido(mensagem_original) or Valida_espaco(mensagem_original):
+        player_atual['socket'].send('ERROR INVALID_FORMAT\r\n'.encode('ascii'))
+        fechar_conexao(player_atual)
+        sys.exit('ERROR INVALID_FORMAT')
 
-def E_UNEXPECTED_MESSAGE(esperado, mensagem, player):
-    if mensagem != esperado  and mensagem.find("ERROR") > -1:
-        player['socket'].send("ERROR UNEXPECTED_MESSAGE\r\n".encode('ascii'))
-        fechar_conexao(player)
-        sys.exit("ERROR UNEXPECTED_MESSAGE")
-#revisar
-def E_INVALID_MASTER_MESSAGE(comando, mensagem, player):
+def E_UNEXPECTED_MESSAGE(esperado, mensagem, player_atual):
+    if mensagem != esperado and mensagem.find('ERROR') == -1:
+        player_atual['socket'].send('ERROR UNEXPECTED_MESSAGE\r\n'.encode('ascii'))
+        fechar_conexao(player_atual)
+        sys.exit('ERROR UNEXPECTED_MESSAGE')
+
+def E_INVALID_MASTER_MESSAGE(comando, mensagem, player, player_atual):
     if comando != 'WORD' or len(mensagem) == 0 or Contem_invalido(mensagem):
-        mensagem_todos_clientes(player, "ERROR INVALID_MASTER_MESSAGE\r\n")
-        fechar_conexao(player)
-        sys.exit("ERROR INVALID_MASTER_MESSAGE")
-#terminar!!!!!
-def E_INVALID_PLAYER_NAME(player):
-    fechar_conexao(player)
-    sys.exit("ERROR INVALID_PLAYER_NAME")
+        mensagem_todos_clientes(player, 'ERROR INVALID_MASTER_MESSAGE\r\n')
+        fechar_conexao(player_atual)
+        sys.exit('ERROR INVALID_MASTER_MESSAGE')
 
-def E_NOT_ENOUGH_PLAYERS(N_player, Min_player, mestre, player):
-    if (N_player < Min_player):
-        mestre['socket'].send("ERROR NOT_ENOUGH_PLAYERS\r\n".encode('ascii'))
-        fechar_conexao(player)
-        sys.exit("ERROR NOT_ENOUGH_PLAYERS")
+def E_INVALID_PLAYER_NAME(player_atual):
+    if player_atual['nome'].find(' ') or player_atual['nome'] == '' or not player_atual['nome'].isalnum():
+        player_atual['socket'].send('ERROR INVALID_PLAYER_NAME\r\n'.encode('ascii'))
+        fechar_conexao(player_atual)
+        sys.exit('ERROR INVALID_PLAYER_NAME')
 
-def E_ALREADY_GUESSED(Palpites, mensagem, player):
-    for x in Palpites:
+def E_NOT_ENOUGH_PLAYERS(mestre, player):
+    if (len(player) < 2):
+        mestre['socket'].send('ERROR NOT_ENOUGH_PLAYERS\r\n'.encode('ascii'))
+        fechar_conexao(mestre)
+        sys.exit('ERROR NOT_ENOUGH_PLAYERS')
+
+def E_ALREADY_GUESSED(palpites, mensagem, player_atual):
+    for x in palpites:
         if x == mensagem:
-            player['socket'].send("ERROR ALREADY_GUESSED\r\n".encode('ascii'))
-            return
+            player_atual['socket'].send('ERROR ALREADY_GUESSED\r\n'.encode('ascii'))
+            return True
+        return False
 
-def E_INVALID_LETTER(mensagem, player):
-    if not mensagem.isalpha:
-        player['socket'].send("ERROR INVALID_LETTER\r\n".encode('ascii'))
-        return
+def E_INVALID_LETTER(mensagem, player_atual):
+    if not mensagem.isalpha():
+        player_atual['socket'].send('ERROR INVALID_LETTER\r\n'.encode('ascii'))
+        return True
+    return False
 
-def E_INVALID_WORD_LENGTH(mensagem, palavra, player):
+def E_INVALID_WORD_LENGTH(mensagem, palavra, player_atual):
     if len(mensagem) != len(palavra):
-        player['socket'].send("ERROR INVALID_WORD_LENGTH\r\n".encode('ascii'))
-        return
-#quando o player sair do jogo ele precisa ser removido da lista de players
-def QUIT():
-    return
+        player_atual['socket'].send('ERROR INVALID_WORD_LENGTH\r\n'.encode('ascii'))
+        return True
+    return False
 
+def QUIT(player,N_player_atual):
+    player[N_player_atual]['socket'].send('OK\r\n'.encode('ascii'))
+    fechar_conexao(player[N_player_atual])
+    player.pop(N_player_atual)
+    return
 
 # Armazenando os parâmetros de execução
 jog_sup = int(sys.argv[1])
@@ -96,36 +111,32 @@ else:
 socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_server.bind((host_server, porta_server))
 socket_server.listen(5)
-print(f"Servidor inicializado na porta {porta_server}")
+print(f'Servidor inicializado na porta {porta_server}')
 
 # Loop principal do jogo
 while True:
     # Aguardando os jogadores entrarem no servidor
-    print("Iniciando novo jogo...")
+    print('Iniciando novo jogo...')
     vidas = 7
     player = []
     try:
         while len(player) < jog_sup:
-            print(f"Aguardando jogador {len(player) + 1}...")
+            print(f'Aguardando jogador {len(player) + 1}...')
             socket_comunicacao, end_client = socket_server.accept()
             mensagem, buffer = ler_msg_completo(socket_comunicacao, b'')
-
-            socket_comunicacao.send("STANDBY\r\n".encode('ascii'))
-
-            nome = mensagem.split()[1]
-            if not nome.isalnum():
-                socket_comunicacao.send("ERROR INVALID_FORMAT\r\n".encode('ascii'))
-                fechar_conexao(player)
-                sys.exit("ERROR INVALID_FORMAT")
-            print(f"Jogador conectado: {nome}")
-            print()
             player.append({
                 'socket': socket_comunicacao,
-                'nome': nome,
+                'nome': mensagem[mensagem.find('')+1:],
                 'buffer_recebimento': buffer
             })
+            E_INVALID_PLAYER_NAME(player)
+            E_INVALID_FORMAT(mensagem, player)
+            E_UNEXPECTED_MESSAGE('NEWPLAYER', mensagem, player)
+            print(f'Jogador conectado: {player['nome']}')
+            print()
+            socket_comunicacao.send('STANDBY\r\n'.encode('ascii'))
     except KeyboardInterrupt:
-        print("\n[Servidor] Interrupção. Fechando recursos...")
+        print('\n[Servidor] Interrupção. Fechando recursos...')
         socket_server.close()
         fechar_conexao(player)
         sys.exit(0)
@@ -136,44 +147,63 @@ while True:
     player.pop(x)
     player.append(mestre)
 
-    print(f"Jogador mestre: {mestre['nome']}")
-    mestre['socket'].send("MASTER\r\n".encode('ascii'))
+    print(f'Jogador mestre: {mestre['nome']}')
+    mestre['socket'].send('MASTER\r\n'.encode('ascii'))
     mensagem, mestre['buffer_recebimento'] = ler_msg_completo(mestre['socket'], mestre['buffer_recebimento'])
+    E_INVALID_MASTER_MESSAGE(mensagem.split[0], mensagem.split[1], mestre)
     palavra = list(mensagem.split()[1].lower())
     if not palavra[0].isalpha():
-        mensagem_todos_clientes(player, "ERROR INVALID_FORMAT\r\n")
+        mensagem_todos_clientes(player, 'ERROR INVALID_FORMAT\r\n')
         fechar_conexao(player)
-        sys.exit("ERROR INVALID_FORMAT")
-    print(f"jogador mestre forneceu a palavra: {''.join(palavra)}")
+        sys.exit('ERROR INVALID_FORMAT')
+    E_UNEXPECTED_MESSAGE('WORD', mensagem.split[0], mestre)
+    print(f'jogador mestre forneceu a palavra: {''.join(palavra)}')
     print()
-    mestre['socket'].send("OK\r\n".encode('ascii'))
-    print("Jogo iniciado com sucesso!")
+    mestre['socket'].send('OK\r\n'.encode('ascii'))
+    print('Jogo iniciado com sucesso!')
     print()
-    mensagem_todos_clientes(player, f"NEWGAME {vidas} {len(palavra)}\r\n")
+    mensagem_todos_clientes(player, f'NEWGAME {vidas} {len(palavra)}\r\n')
 
     # Parte principal do jogo
     estado = False
     atual = 0
     status = list('_' * len(palavra))
+    lista_palpites = list()
     while True:
 
         if atual >= len(player)- 1:
             atual = 0
         try:
-            print(f"exibindo o valor atual {atual}")
             player_atual = player[atual]
-            print(f"Vez do jogador {player_atual['nome']}")
-            player[atual]['socket'].send("YOURTURN\r\n".encode('ascii'))
+            print(f'Vez do jogador {player_atual['nome']}')
+            player[atual]['socket'].send('YOURTURN\r\n'.encode('ascii'))
             mensagem, player_atual['buffer_recebimento'] = ler_msg_completo(player_atual['socket'],
                                                                             player_atual['buffer_recebimento'])
             comando_palpite = mensagem.split()[1]
             palpite = list(mensagem.split()[2].lower())
+            
+            if mensagem.spli[0] == 'QUIT':
+                QUIT(player_atual, atual)
+                E_NOT_ENOUGH_PLAYERS(mestre, player_atual)
+            E_UNEXPECTED_MESSAGE('GUESS', mensagem.split[0])
+            if E_INVALID_WORD_LENGTH(palpite, palavra, player_atual) or E_INVALID_LETTER(palpite, player_atual):
+                vidas -= 1
+                comando_palpite = 'invalido'
+                estado = True
+            E_INVALID_FORMAT(mensagem, player_atual)
+            lista_palpites.append(palpite)
+            if E_ALREADY_GUESSED(lista_palpites, palpite):
+                vidas -= 1
+                comando_palpite = 'invalido'
+                estado = True
+
+            
         except KeyboardInterrupt:
-            print("\n[Servidor] Interrupção. Fechando recursos...")
+            print('\n[Servidor] Interrupção. Fechando recursos...')
             socket_server.close()
             fechar_conexao(player)
             sys.exit(0)
-        player_atual['socket'].send("OK\r\n".encode('ascii'))
+        player_atual['socket'].send('OK\r\n'.encode('ascii'))
 
         if comando_palpite == 'LETTER':
             print(f"Processando palpite de letra: '{''.join(palpite)}'")
@@ -195,21 +225,21 @@ while True:
                 vidas -= 1
 
         if estado:
-            print(f"Jogador {player_atual['nome']} advinhou a palvra!")
-            mensagem_todos_clientes(player, f"GAMEOVER WIN {player_atual['nome']} {''.join(palavra)}\r\n")
+            print(f'Jogador {player_atual['nome']} advinhou a palvra!')
+            mensagem_todos_clientes(player, f'GAMEOVER WIN {player_atual['nome']} {''.join(palavra)}\r\n')
             break
 
         if vidas == 0:
-            print(f"A palavra não foi advinhada. Último jogador: {player_atual['nome']}")
-            mensagem_todos_clientes(player, f"GAMEOVER LOSE {player_atual['nome']} {''.join(palavra)}\r\n")
+            print(f'A palavra não foi advinhada. Último jogador: {player_atual['nome']}')
+            mensagem_todos_clientes(player, f'GAMEOVER LOSE {player_atual['nome']} {''.join(palavra)}\r\n')
             break
 
-        print(f"Continuando jogo. Estado atual: {''.join(status)}, vidas restantes: {vidas}.")
-        mensagem_todos_clientes(player, f"STATUS {vidas} {''.join(status)} {player[atual]['nome']} {''.join(palpite)}\r\n")
+        print(f'Continuando jogo. Estado atual: {''.join(status)}, vidas restantes: {vidas}.')
+        mensagem_todos_clientes(player, f'STATUS {vidas} {''.join(status)} {player[atual]['nome']} {''.join(palpite)}\r\n')
         atual += 1
 
     # Finalizando o jogo
-    print("Finalizando jogo...")
+    print('Finalizando jogo...')
     fechar_conexao(player)
     print()
     print()
